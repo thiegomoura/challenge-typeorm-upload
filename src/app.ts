fdee501517fd72a5b451b6bd98493ca9a6a34998
thiegomoura <thi-ego@hotmail.com>
2021-06-04 16:52:59 -0400

    add cors and relations eager

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import 'reflect-metadata';
+import cors from 'cors';
 import 'dotenv/config';
+import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -13,6 +14,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
